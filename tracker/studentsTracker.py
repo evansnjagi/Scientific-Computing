10eaa1e5d8d5d87e7eec3976cb647a99ed03f8de
Evanskarago <148349906+evansnjagi@users.noreply.github.com>
2026-02-02 07:19:26 +0000

    Added new features in add students record

--- tracker/studentsTracker.py
+++ tracker/studentsTracker.py
@@ -1,6 +1,14 @@
 # Implementing a students tracker program.
 # Import modules
 import pandas as pd
+import logging
+
+# Instantiating logging
+logging.basicConfig(
+    filename = "studentsTracker.log",
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s'
+)
 
 # Add a students recored fuction
 def addRecord(students, name, subject, score):
@@ -12,7 +20,7 @@ def addRecord(students, name, subject, score):
             "Score": score
         }
     )
-    return True
+    logging.debug(f"Added the {name} {subject} score as: {score}")
 
 # Print students record function
 def printRecord(students):
@@ -42,39 +50,68 @@ def highestScoring(students, subject):
     for i in range(len(totalScores)):
         if highestScore < totalScores[i]:
             highestScores = totalScores[i]
-    topStudents = [student for student in students[student]["Scores"] == highestScore]
+    topStudents = [student for student in students[students]["Scores"] == highestScore]
     print(f"Top student in {subject} is {topStudents} with {highestScore}")
 
 # Main function
 def main():
-    print("\n Students Performance Tracker")
+    logging.debug("Started the main program")
+    print("\nStudents Performance Tracker")
     students = []
 
     while True:
-        print("Choose an option")
-        print("1. Add a students record")
-        print("2. Print all students record")
-        print("3. Compute average score")
-        print("4. Filter students by 'name' or by 'subject'")
-        print("5. Get the highest student")
-        print("q. quit")
-
-        print("\n\n")
+        print("\nChoose an option")
+        print("\t1. Add a students record")
+        print("\t2. Print all students record")
+        print("\t3. Compute average score")
+        print("\t4. Filter students by 'name' or by 'subject'")
+        print("\t5. Get the highest student")
+        print("\tq. quit")
+        print("\n")
         choice = input("Enter your choice: ")
         if choice == "1":
-            print("\n Enter records")
-            print("c. Change subject, students and marks")
-            print("n. Add students and marks")
-            print("r. return")
+            logging.debug("Adding a students record section")
+            print("\tEnter records")
+            print("\tc. Change subject and marks")
+            print("\tn. Add students and marks")
+            print("\tr. return")
+
+            # Getting users input on their choice record and the subject to recotd
             choiceRecord = input("Enter what you want to do: ")
             subject = input("Enter subject: ")
-            if choiceRecord == "n":
-                name = input("Enter students name: ")
-                score = input("Enter score: ")
-                addRecord(students, name, subject, score)
-            if choiceRecord == "c":
-                subject = input("Enter a new subject")
-                while True:
+
+            changeRecord = True
+            while changeRecord:
+                if choiceRecord == "n":
                     name = input("Enter students name: ")
-                    score = input("Enter score: ")
+
+                    # Validate scores
+                    try:
+                        score = int(input("Enter students scores: "))
+                        addRecord(students, name, subject, score)
+                        logging.info("added students record successfully")
+
+                        # Continue or go back to something else
+                        escapeKey = True
+                        while escapeKey:
+                            backContinue = input("go back (r) or continue (c): ")
+                            if backContinue == "c":
+                                escapeKey = False
+                                continue
+
+                            elif backContinue == "r":
+                                changeRecord = False
+                                escapeKey = False
+
+                            else:
+                                print("Enter a valid escape key!")
+                                logging.debug("Handle wrong escape key by repromting the user")
+                                continue
+                    except Exception as err:
+                        logging.debug("Handled exception error gracefuly")
+                        print(f"Invalid score type: {err}")
+                        print("Try again! Score was never recorded")
+                        continue
+                
+
 main()
