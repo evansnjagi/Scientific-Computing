046c4684b27dae831dab16ee7c127f7ac7355b27
Evanskarago <148349906+evansnjagi@users.noreply.github.com>
2026-02-04 15:00:41 +0000

    Adding students subject, scores or names

--- tracker/studentsTracker.py
+++ tracker/studentsTracker.py
@@ -9,7 +9,20 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s'
 )
-
+# Print function
+def printFace():
+    while True:
+        print("\nChoose an option")
+        print("\t1. Add a students record")
+        print("\t2. Print all students record")
+        print("\t3. Compute average score")
+        print("\t4. Filter students by 'name' or by 'subject'")
+        print("\t5. Get the highest student")
+        print("\tq. quit")
+        print("\n")
+        choice = input("Enter your choice: ")
+        return choice
+    
 # Add a students recored fuction
 def addRecord(students, name, subject, score):
     """Students is an empty list"""
@@ -53,65 +66,80 @@ def highestScoring(students, subject):
     topStudents = [student for student in students[students]["Scores"] == highestScore]
     print(f"Top student in {subject} is {topStudents} with {highestScore}")
 
+# Choice one function 
+def choiceOne(students):
+    logging.debug("Adding a students record section")
+    print("\n-----< You are on Enter Students Record Section >-----")
+    print("\ts. Add subject and marks")
+    print("\tn. Add students and marks")
+    print("\tr. return")
+
+    userChoice = input("\nEnter your choice: ")
+    if userChoice == "s":
+        studentsName = input("Enter students name: ")
+        student = True
+        while student:
+            # Get the subject
+            subject = input("Enter Subject: ")
+
+            # get the marks
+            try:
+                score = int(input(f"Enter {studentsName}, {subject} scores: "))
+                addRecord(students, studentsName, subject, score)
+
+                # Getting user input if they want to continue or return 
+                userConcent = input("continue (c) or return (r): ")
+
+                if userConcent == "c":
+                    continue
+                elif userConcent == "r":
+                    student = False
+                else:
+                    print("Enter a valid escape character (c or r)")
+                    continue
+
+            except Exception as err:
+                print("Invalid score! Try again.")
+                logging.error(f"Invalid score type inputed {err}")
+                continue
+    elif userChoice == "n":
+        subjectName = input("Enter subject: ")
+        subject = True
+        while subject:
+            studentName = input("Enter students name: ")
+            try:
+                score = int(input(f"Enter {studentName}, {subjectName} scores: "))
+                addRecord(students, studentName, subjectName, score)
+
+                userConcent = input("continue (c) or return (r): ")
+
+                if userConcent == "c":
+                    continue
+                elif userConcent == "r":
+                    subject = False
+                else:
+                    print("Enter a valid escape character (c or r)")
+                    continue
+            except Exception as err:
+                print("Invalid score! Try again.")
+                logging.error(f"Invalid score input with the error {err}.")
+                continue      
+    elif userChoice == "r":
+        return True
+    else:
+        print("Invalid input character! Try again.")      
 # Main function
 def main():
     logging.debug("Started the main program")
     print("\nStudents Performance Tracker")
     students = []
 
-    while True:
-        print("\nChoose an option")
-        print("\t1. Add a students record")
-        print("\t2. Print all students record")
-        print("\t3. Compute average score")
-        print("\t4. Filter students by 'name' or by 'subject'")
-        print("\t5. Get the highest student")
-        print("\tq. quit")
-        print("\n")
-        choice = input("Enter your choice: ")
-        if choice == "1":
-            logging.debug("Adding a students record section")
-            print("\tEnter records")
-            print("\tc. Change subject and marks")
-            print("\tn. Add students and marks")
-            print("\tr. return")
+    # Envoking the UI function
+    choice = printFace()
+    if choice == "1":
+        choiceOne(students)
+        if choiceOne(students):
+            printFace()
 
-            # Getting users input on their choice record and the subject to recotd
-            choiceRecord = input("Enter what you want to do: ")
-            subject = input("Enter subject: ")
-
-            changeRecord = True
-            while changeRecord:
-                if choiceRecord == "n":
-                    name = input("Enter students name: ")
-
-                    # Validate scores
-                    try:
-                        score = int(input("Enter students scores: "))
-                        addRecord(students, name, subject, score)
-                        logging.info("added students record successfully")
-
-                        # Continue or go back to something else
-                        escapeKey = True
-                        while escapeKey:
-                            backContinue = input("go back (r) or continue (c): ")
-                            if backContinue == "c":
-                                escapeKey = False
-                                continue
-
-                            elif backContinue == "r":
-                                changeRecord = False
-                                escapeKey = False
-
-                            else:
-                                print("Enter a valid escape key!")
-                                logging.debug("Handle wrong escape key by repromting the user")
-                                continue
-                    except Exception as err:
-                        logging.debug("Handled exception error gracefuly")
-                        print(f"Invalid score type: {err}")
-                        print("Try again! Score was never recorded")
-                        continue
-                
 
 main()
